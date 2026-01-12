6100614655452a1f7c2527a9f0087ae089dd0cf3
Henry Asante <yourhenryasante@gmail.com>
2026-01-12 04:20:57 +0000

    Add first python program app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+name = "Ramin Khadir"
+age = 33
+height = float(input("Enter your height: "))
+is_student = False
+password = 234134
+username= input("Enter your name: ")
+if username == name:
+    print ()
+else:
+    print ("wrong")
+log_in = int(input("Enter your password: "))
+if log_in == password:
+    print("You nailed it")
+else:
+    print("Almost there.")
